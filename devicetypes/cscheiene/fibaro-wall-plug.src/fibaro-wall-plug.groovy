f7be1d51d6855224d016e41c21bc400d310a6b33
Christian Scheiene <cscheiene@gmail.com>
2016-11-15 07:53:31 +0100

    More config options added

--- devicetypes/cscheiene/fibaro-wall-plug.src/fibaro-wall-plug.groovy
+++ devicetypes/cscheiene/fibaro-wall-plug.src/fibaro-wall-plug.groovy
@@ -79,27 +79,27 @@ metadata {
 	}
     preferences {
         input name: "par16", type: "number", description: "Enter number", required: true,
-        title: "Remember device status after power failure\n\n" +
+        title: "Remember device status after power failure. Default: 1\n\n" +
         "0 - Wall Plug does not memorize its state after a power failure. Connected device will be off after the power supply is reconnected\n" +
-        "1 - Wall Plug memorizes its state after a power failure.(Default)"
+        "1 - Wall Plug memorizes its state after a power failure."
         
         input name: "par40", type: "number", description: "Enter number", required: true,
-        title: "Immediate power report\n\n" +
-        "Available settings: 1 - 100 (%). Default 80"
+        title: "Immediate power report. Default: 80\n\n" +
+        "Available settings: 1 - 100 (%)."
                 
         input name: "par42", type: "number", description: "Enter number", required: true,
-        title: "Standard power reporting\n\n" +
-        "Available settings: 1 - 100 (%). Default 15"
+        title: "Standard power reporting. Default: 15\n\n" +
+        "Available settings: 1 - 100 (%)"
                         
         input name: "par43", type: "number", description: "Enter number", required: true,
-        title: "Standard power reporting frequency\n\n" +
-        "Available settings: 1 - 254 (s) Default 30"
+        title: "Standard power reporting frequency. Default: 30\n\n" +
+        "Available settings: 1 - 254 (s)"
         
         input name: "par61", type: "number", description: "Enter number", required: true,
-        title: "LED ring illumination colour when device is ON\n\n" +
+        title: "LED ring illumination colour when device is ON. Default: 1\n\n" +
         "Available settings:\n" +
         "0 - LED ring illumination colour changes in predefined steps, depending on power consumption changes\n" +
-        "1 - LED ring illumination colour changes continuously, using full spectrum of available colorus, depending on power consumption changes(default)\n" +
+        "1 - LED ring illumination colour changes continuously, using full spectrum of available colorus, depending on power consumption changes\n" +
         "2 - White illumination\n" +
 		"3 - Red illumination\n" +
 		"4 - Green illumination\n" +
@@ -110,7 +110,7 @@ metadata {
         "9 - illumination turned off completely"
         
         input name: "par62", type: "number", description: "Enter number", required: true,
-        title: "LED ring illumination colour when device is OFF\n\n" +
+        title: "LED ring illumination colour when device is OFF. Default: 8\n\n" +
         "Available settings:\n" +
         "0 - LED ring is illuminated with a color corresponding to the last measured power, before the controlled device was turned off\n" +
         "1 - White illumination\n" +
@@ -121,6 +121,12 @@ metadata {
 		"6 - Cyan (Greenish blue) illumination\n" + 
         "7 - Magenta (Purplish red) illumination\n" + 
         "8 - illumination turned off completely"
+        
+        input name: "par70", type: "number", description: "Enter number", required: true,
+        title: "Oveload safety switch. Default: 65 535 (6 553,5W)\n\n" +
+        "Available settings:\n" +
+        "10 - 65 535 (1W - 6 553,5W)\n" +
+        "Value higher than 32 000 (3 200W) turns the overload safety switch off, i.e. this functionality is turned off by default."        
     }
 }
 
@@ -233,7 +239,8 @@ def configure() {
         zwave.configurationV1.configurationSet(parameterNumber: 42, size: 1, scaledConfigurationValue: par42.toInteger()).format(), 	// Standard power reporting. Available settings: 1 - 100 (%). Default 15
         zwave.configurationV1.configurationSet(parameterNumber: 43, size: 1, scaledConfigurationValue: par43.toInteger()).format(), 	// Standard power reporting frequency. Available settings: 1 - 254 (s) Default 30
         zwave.configurationV1.configurationSet(parameterNumber: 61, size: 1, scaledConfigurationValue: par61.toInteger()).format(),     // LED Ring when ON
-        zwave.configurationV1.configurationSet(parameterNumber: 62, size: 1, scaledConfigurationValue: par62.toInteger()).format(),     // LED Ring when ON        
+        zwave.configurationV1.configurationSet(parameterNumber: 62, size: 1, scaledConfigurationValue: par62.toInteger()).format(),     // LED Ring when OFF
+        zwave.configurationV1.configurationSet(parameterNumber: 70, size: 2, scaledConfigurationValue: par70.toInteger()).format(),     // Oveload safety switch         
         zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]).format(),
         zwave.associationV1.associationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]).format(),
         zwave.associationV1.associationSet(groupingIdentifier:3, nodeId:[zwaveHubNodeId]).format(),
