a8643a09a393dcbe8bf42b6c38520243c50fa7f5
Christian Scheiene <cscheiene@gmail.com>
2017-02-16 10:48:52 +0100

    Updated layout

--- devicetypes/cscheiene/netatmo-outdoor-module.src/netatmo-outdoor-module.groovy
+++ devicetypes/cscheiene/netatmo-outdoor-module.src/netatmo-outdoor-module.groovy
@@ -26,7 +26,23 @@ metadata {
 		// TODO: define status and reply messages here
 	}
 
-	tiles {
+	tiles (scale: 2) {
+		multiAttributeTile(name:"main", type:"generic", width:6, height:4) {
+			tileAttribute("temperature", key: "PRIMARY_CONTROL") {
+            	attributeState "temperature",label:'${currentValue}°', icon:"st.Weather.weather2", backgroundColors:[
+                	[value: 32, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 92, color: "#d04e00"],
+					[value: 98, color: "#bc2323"]
+				]
+            }
+            tileAttribute ("humidity", key: "SECONDARY_CONTROL") {
+				attributeState "humidity", label:'Humidity: ${currentValue}%'
+			}
+		} 
 		valueTile("temperature", "device.temperature", width: 2, height: 2, canChangeIcon: false) {
  			state("temperature", label: '${currentValue}°', icon:"st.Weather.weather2", backgroundColors: [
  				[value: 31, color: "#153591"],
@@ -42,11 +58,11 @@ metadata {
  		valueTile("humidity", "device.humidity", inactiveLabel: false) {
  			state "humidity", label:'${currentValue}%', unit:"Humidity"
  		}
- 		standardTile("refresh", "device.thermostatMode", inactiveLabel: false, decoration: "flat") {
+ 		standardTile("refresh", "device.thermostatMode", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
  			state "default", action:"device.poll", icon:"st.secondary.refresh"
  		}
- 		main (["temperature", "humidity"])
- 		details(["temperature", "humidity", "refresh"])
+ 		main (["main"])
+ 		details(["main", "refresh"])
 	}
 }
 
