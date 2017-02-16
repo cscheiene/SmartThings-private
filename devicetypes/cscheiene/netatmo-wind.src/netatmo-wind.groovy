8852ae2272b7a2eeb2e6041aeffa6aafa7a5502e
Christian Scheiene <cscheiene@gmail.com>
2017-02-16 10:12:36 +0100

    Updated layout

--- devicetypes/cscheiene/netatmo-wind.src/netatmo-wind.groovy
+++ devicetypes/cscheiene/netatmo-wind.src/netatmo-wind.groovy
@@ -33,30 +33,46 @@ metadata {
 		// TODO: define status and reply messages here
 	}
 
-	tiles {
- 		valueTile("WindStrength", "WindStrength", canChangeIcon: false, inactiveLabel: false) {
+	tiles (scale: 2) {
+		multiAttributeTile(name:"main", type:"generic", width:6, height:4) {
+			tileAttribute("WindStrength", key: "PRIMARY_CONTROL") {
+            	attributeState "WindStrength",label:'${currentValue}', icon:"st.Weather.weather1", backgroundColors:[
+                	[value: 32, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 92, color: "#d04e00"],
+					[value: 98, color: "#bc2323"]
+				]
+            }
+            tileAttribute ("WindAngle", key: "SECONDARY_CONTROL") {
+				attributeState "WindAngle", label:'${currentValue}° Average'
+			}
+		}        
+ 		valueTile("WindStrength", "WindStrength", width: 2, height: 2, canChangeIcon: false, inactiveLabel: false) {
  			state "default", label:'${currentValue} Average', icon:"st.Weather.weather1"
  		}
  		valueTile("WindAngle", "WindAngle", inactiveLabel: false) {
  			state "default", label:'${currentValue}° Average'
  		}
- 		valueTile("GustStrength", "GustStrength", inactiveLabel: false) {
+ 		valueTile("GustStrength", "GustStrength", width: 2, height: 1, inactiveLabel: false) {
  			state "default", label:'${currentValue} Gust'
  		}
-        valueTile("GustAngle", "GustAngle", inactiveLabel: false) {
+        valueTile("GustAngle", "GustAngle", width: 2, height: 1, inactiveLabel: false) {
  			state "default", label:'${currentValue}° Gust'            
  		}
-        valueTile("max_wind_str", "max_wind_str", inactiveLabel: false) {
+        valueTile("max_wind_str", "max_wind_str", width: 2, height: 1, inactiveLabel: false) {
  			state "default", label:'${currentValue} Todays Max'            
  		}
-        valueTile("units", "units", inactiveLabel: false) {
+        valueTile("units", "units", width: 2, height: 1, inactiveLabel: false) {
  			state "default", label:'Units: ${currentValue}'            
  		}          
- 		standardTile("refresh", "Netatmo Wind", inactiveLabel: false, decoration: "flat") {
+ 		standardTile("refresh", "Netatmo Wind", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
  			state "default", action:"refresh.poll", icon:"st.secondary.refresh"
  		}      
- 		main (["WindStrength", "WindAngle", "GustStrength"])
- 		details(["WindStrength", "WindAngle", "units", "GustStrength", "GustAngle", "max_wind_str", "refresh" ])
+ 		main (["main"])
+ 		details(["main", "GustStrength", "GustAngle", "max_wind_str", "units", "refresh" ])
 	}
 }
 
